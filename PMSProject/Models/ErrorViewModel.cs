5b54baa36c3322b77113e7ee1efac5c71c17bfbc
Ashbel <Ashbel@C11107>
2026-07-15 11:13:09 +0300

    Add project files.

---
+++ PMSProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PMSProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
